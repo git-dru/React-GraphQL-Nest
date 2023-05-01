6689b0391918cd0e3c626b2b074016a85abb895b
git-dru <mastercrypto.us@gmail.com>
2023-05-01 13:33:28 +0100

    add track model

---
+++ src/track.model.ts
@@ -0,0 +1,19 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class Track {
+  @Field()
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  price: number;
+
+  @Field()
+  duration: number;
+
+  @Field()
+  genre: number;
+}
